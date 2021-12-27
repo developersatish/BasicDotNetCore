bb72f5e6021c804feffbe8d0cb33528dd18dacb4
developersatish <satishsingh.net@outlook.com>
2021-12-27 16:08:39 +0530

    new changes

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -1,12 +1,16 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ExploreCalifornia
 {
@@ -20,9 +24,33 @@ namespace ExploreCalifornia
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection services)
         {
             //services.AddRazorPages();
+
+            services.AddTransient<FormattingService>();
+
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                DeveloperExceptions = Configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
+            });
+
+            services.AddDbContext<BlogDataContext>(options =>
+            {
+                var connectionString = Configuration.GetConnectionString("BlogDataContext");
+                options.UseSqlServer(connectionString);
+            });
+       
+
+
+            services.AddDbContext<IdentityDataContext>(options =>
+            {
+                var connectionString = Configuration.GetConnectionString("IdentityDataContext");
+                options.UseSqlServer(connectionString);
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDataContext>();
 
             services.AddMvc(options=> options.EnableEndpointRouting=false);
             
@@ -56,6 +84,7 @@ namespace ExploreCalifornia
                 routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
 
             });
+            app.UseAuthentication();
             app.UseFileServer();
             //app.UseStaticFiles();
 
